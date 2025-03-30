572681a1c739c51ebec2fca597bf9c0c96b29711
red-malone <rohit.psudarsanan@gmail.com>
2025-03-30 20:59:51 +0530

    firebase addition

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:photos/firebase_options.dart';
 import 'package:photos/provider/photo_provider.dart';
 import 'package:photos/screen/home.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MultiProvider(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(
+    MultiProvider(
     providers: [
       ChangeNotifierProvider(
         create: (context) => PhotoProvider(),
--- lib/provider/photo_provider.dart
+++ lib/provider/photo_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:photos/models/photos.dart';
 import 'package:http/http.dart' as http;
@@ -7,6 +8,8 @@ import 'package:http/http.dart' as http;
 class PhotoProvider with ChangeNotifier {
   List<Photos> _photos = [];
   List<int> _favoritePhotos = [];
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   List<Photos> get photos => _photos;
   List<int> get favoritePhotos => _favoritePhotos;
 
@@ -17,17 +20,26 @@ class PhotoProvider with ChangeNotifier {
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
       _photos = data.map((item) => Photos.fromJson(item)).take(50).toList();
+      await _loadFavourites();
       notifyListeners();
     } else {
       throw Exception('Failed to load photos');
     }
   }
 
-  void togglefav(int id) {
+  Future<void> _loadFavourites() async {
+    final snapshot = await _firestore.collection('favourites').get();
+    _favoritePhotos = snapshot.docs.map((doc) => int.parse(doc.id)).toList();
+    notifyListeners();
+  }
+
+  void togglefav(int id) async {
     if (_favoritePhotos.contains(id)) {
       _favoritePhotos.remove(id);
+      await _firestore.collection('favourites').doc(id.toString()).delete();
     } else {
       _favoritePhotos.add(id);
+      await _firestore.collection('favourites').doc(id.toString()).set({});
     }
     notifyListeners();
   }
